d3aea1fe9a271852a79c8cde69edfa3daa25cfc5
varges78 <87320481+varges78@users.noreply.github.com>
2022-07-27 09:33:00 +0300

    Update livebets.py

--- livebets.py
+++ livebets.py
@@ -1,4 +1,4 @@
-﻿from telethon import TelegramClient, events
+from telethon import TelegramClient, events
 
 api_id = '19156942'
 api_hash = '341ed94ec15ab328b837b4ce4c3ced55'
@@ -9,28 +9,11 @@ client.start()
 # Pocket Option Official Signal Bot
 @client.on(events.NewMessage(1385109737))
 async def main(event):
-    # @client.message_handler(content_types=['text'])
-    # def send_text(message):
-        # string = message.text.lower()
-        # substring = 'символ'
-        # if string.find(substring) > -1:
-            await client.send_message(-1001548724631, event.message)
-
-# ORLOW TRADING CLUB
-@client.on(events.NewMessage(-1001226882428))
-async def main(event):
-     await client.send_message(-1001548724631, event.message)
-
+            await client.send_message(-1001166633924, event.message) # WINBINARY
+            await client.send_message(-1001547454993, event.message) # ПРОФИТНЫЕ СИГНАЛЫ
 
 # Live прогнозы и ставки на спорт
-@client.on(events.NewMessage(-1001226882428))
+@client.on(events.NewMessage(-1001342598437))
 async def main(event):
-     await client.send_message(-1001342598437, event.message)
-
-
-# ФАРМИМ БАБЛО
-@client.on(events.NewMessage(-1001358758220))
-async def main(event):
-     await client.send_message(-1001548724631, event.message)
-
-client.run_until_disconnected()
+     await client.send_message(-1001094033192, event.message) # VARGES CLUB
+     await client.send_message(-1001250994275, event.message) # BEARETOR BETS
